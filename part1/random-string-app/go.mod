e407917c858af90cc128f2308077e2cc2d8cc20a
Sam Akers <sam.akers1@gmail.com>
2025-01-23 13:29:35 +0000

    add random string logic

---
+++ part1/random-string-app/go.mod
@@ -0,0 +1,3 @@
+module github.com/samakers/devopswithkubernetes
+
+go 1.21.6
--- part1/random-string-app/main.go
+++ part1/random-string-app/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"time"
+)
+
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, length)
+	for i := range result {
+		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		result[i] = charset[n.Int64()]
+	}
+	return string(result)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	randomString := generateRandomString(10)
+
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Printf("%s - Random string: %s\n",
+				time.Now().Format("2006-01-02 15:04:05"),
+				randomString)
+		}
+	}
 }
